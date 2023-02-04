d2b9600110ffcbcbd66c637a401afb68a23f3257
pranto mollick <prantocse01@gmail.com>
2023-02-05 01:34:36 +0600

    types of events and events handler

--- script.js
+++ script.js
@@ -136,3 +136,17 @@ btnScrollTo.addEventListener('click', function (e) {
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
