815568efcbbeb45fd96f56bc62f7cef00b2197cc
pranto mollick <prantocse01@gmail.com>
2023-02-05 21:15:18 +0600

    building tabbed component

--- script.js
+++ script.js
@@ -77,6 +77,30 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+//Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  //Gurd Clause
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(tc => tc.classList.remove('operations__content--active'));
+
+  //activate tab
+  clicked.classList.add('operations__tab--active');
+
+  //Activate content Area
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
 /*
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
@@ -200,7 +224,7 @@ document.querySelector('.nav').addEventListener(
   },
   true
 );
-*/
+
 
 //DOM Traversing
 
@@ -232,3 +256,4 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
+*/
