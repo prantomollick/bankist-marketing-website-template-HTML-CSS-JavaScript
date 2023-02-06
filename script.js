3b070e3e1dbc45d9e05408d006c11cd2b7f58d74
pranto mollick <prantocse01@gmail.com>
2023-02-06 16:13:17 +0600

    building slider

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
 });
 
 //Lazy loading images
@@ -191,7 +191,6 @@ const imgTargets = document.querySelectorAll('img[data-src]');
 
 const loadImg = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
 
@@ -213,6 +212,51 @@ const imgObserver = new IntersectionObserver(loadImg, {
 
 imgTargets.forEach(img => imgObserver.observe(img));
 
+//////////////////////////////////////////
+/////Slider
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const slider = document.querySelector('.slider');
+
+// slider.style.transform = 'scale(0.2)';
+// slider.style.overflow = 'visible';
+
+let curSlide = 0;
+let maxSlide = slides.length - 1;
+// slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+//0%, 100%, 200%, 300%, 400%
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+//next Slide
+const nextSlide = function () {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
 /*
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
